f37349c15bc006959927e4091369685de5d4e8d3
bmdx26 <poseidonx26@gmail.com>
2024-07-06 17:39:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const loadingText = document.getElementById('loading-text');
+  const loadingScreen = document.getElementById('loading-screen');
+  const pages = document.querySelectorAll('.page');
+  const navBar = document.querySelector('.nav-bar');
+
+  setTimeout(function() {
+      loadingText.classList.add('shrink');
+      loadingScreen.style.opacity = 0;
+      pages.forEach(page => page.style.opacity = 1);
+      navBar.style.opacity = 1;
+      document.body.style.overflow = 'auto';
+  }, 2500);
+});
